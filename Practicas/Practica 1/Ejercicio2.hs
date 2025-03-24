9d1d2a2d62408f0a572b649ea1bfebe46d5cb4db
Philips <142275614+philips314@users.noreply.github.com>
2025-03-24 15:15:13 -0300

    Add files via upload

---
+++ Practicas/Practica 1/Ejercicio2.hs
@@ -0,0 +1,14 @@
+-- I. curry, dada una función de dos argumentos, devuelve su equivalente currificada.
+curry :: ((a, b) -> c) -> a -> b -> c
+curry = \f -> \x -> \y -> f (x, y)
+
+
+-- II. uncurry, dada una función currificada de dos argumentos, devuelve su versión no currificada equivalente.
+uncurry :: (a -> b -> c) -> ((a,b) -> c)
+uncurry = \f -> \(x, y) -> f x y
+
+
+-- III. curryN, dada una función de un número arbitrario de argumentos y devuelva su versión currificada.
+-- No puede hacerse en Haskell porque no hay manera 'genérica' para cualquier cantidad de argumentos, ya que Haskell no tiene introspección sobre la cantidad de argumentos de una función.
+-- curryN :: ((a1, a2,..., an) -> b) -> a1 -> a2 -> ... -> an -> b
+-- curryN f a1 a2 ... an = f (a1, a2,..., an)
