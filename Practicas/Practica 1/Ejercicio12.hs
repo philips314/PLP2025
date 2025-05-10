ec4e6c1f9dcd6638d0cd3eb94da886467225672a
Philips <142275614+philips314@users.noreply.github.com>
2025-05-10 14:38:24 -0300

    Update Ejercicio12.hs

--- Practicas/Practica 1/Ejercicio12.hs
+++ Practicas/Practica 1/Ejercicio12.hs
@@ -4,11 +4,13 @@ data AB a = Nil | Bin (AB a) a (AB a)
 
 --I. foldAB y recAB
 -- recr :: (head -> tail -> recTail -> recConHead) -> casoBase -> Lista -> RES
+-- foldAB :: casoBase -> (recIzq -> raiz -> recDer -> recCompleta) -> Arbol -> res
+-- recAB :: casoBase -> (Arbol -> recIzq -> raiz -> recDer -> recCompleta) -> Arbol -> res
 
-foldAB ::  b -> (a -> b -> b -> b) -> AB a -> b
+foldAB ::  b -> (b -> a -> b -> b) -> AB a -> b
 foldAB  cNil cBin t = case t of
     Nil -> cNil
-    Bin i v d -> cBin v (rec i) (rec d)
+    Bin i v d -> cBin  (rec i)  v (rec d)
         where rec = foldAB cNil cBin
 
 recAB :: b -> (AB a -> b -> a -> b -> b) -> AB a -> b
@@ -24,15 +26,15 @@ esNil Nil = True
 esNil (Bin i v d) = False
 
 altura :: AB a -> Int
-altura = foldAB 0 (\_ ri rd -> 1 + max ri rd)
+altura = foldAB 0 (\ri _ rd -> 1 + max ri rd)
 
 cantNodos :: AB a -> Int
-cantNodos = foldAB 0 (\_ ri rd -> 1 + ri + rd)
+cantNodos = foldAB 0 (\ri _ rd -> 1 + ri + rd)
 
 --III. mejorSegun :: (a -> a -> Bool) -> AB a -> a
 mejorSegunAB :: (a -> a -> Bool) -> AB a -> a
 mejorSegunAB _ Nil = error "AB Nulo"
-mejorSegunAB f (Bin izq raiz der) = foldAB raiz (\root ri rd -> compararTres f root ri rd) (Bin izq raiz der)
+mejorSegunAB f (Bin izq raiz der) = foldAB raiz (\ri root rd -> compararTres f root ri rd) (Bin izq raiz der)
  
 compararTres :: (a -> a -> Bool) -> a -> a -> a -> a
 compararTres f root rootIzq rootDer = if (f root rootIzq && f root rootDer) then root else (if f rootIzq rootDer then rootIzq else rootDer) 
